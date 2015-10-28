e09f62e6cf40a96c748914da4dc76d61aa63ea06
IdemIsDito <jeroenrwever@gmail.com>
2015-10-28 12:59:05 +0100

    added aurelia-validation plugin + activated aurelia css animations

--- config.js
+++ config.js
@@ -36,10 +36,11 @@ System.config({
     "aurelia-templating-binding": "github:aurelia/templating-binding@0.15.0",
     "aurelia-templating-resources": "github:aurelia/templating-resources@0.15.2",
     "aurelia-templating-router": "github:aurelia/templating-router@0.16.1",
-    "babel": "npm:babel-core@5.8.24",
-    "babel-runtime": "npm:babel-runtime@5.8.20",
+    "aurelia-validation": "github:aurelia/validation@0.4.0",
+    "babel": "npm:babel-core@5.8.30",
+    "babel-runtime": "npm:babel-runtime@5.8.29",
     "bootstrap": "github:twbs/bootstrap@3.3.5",
-    "core-js": "npm:core-js@0.9.18",
+    "core-js": "npm:core-js@1.2.3",
     "fetch": "github:github/fetch@0.9.0",
     "firebase": "github:firebase/firebase-bower@2.3.1",
     "firenext": "npm:firenext@3.1.10",
@@ -50,6 +51,12 @@ System.config({
     "github:aurelia/animator-css@0.16.0": {
       "aurelia-metadata": "github:aurelia/metadata@0.8.0",
       "aurelia-templating": "github:aurelia/templating@0.15.3"
+    },
+    "github:aurelia/binding@0.10.2": {
+      "aurelia-metadata": "github:aurelia/metadata@0.9.0",
+      "aurelia-pal": "github:aurelia/pal@0.2.0",
+      "aurelia-task-queue": "github:aurelia/task-queue@0.8.0",
+      "core-js": "npm:core-js@0.9.18"
     },
     "github:aurelia/binding@0.9.1": {
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.10.1",
@@ -74,6 +81,12 @@ System.config({
     "github:aurelia/dependency-injection@0.10.1": {
       "aurelia-logging": "github:aurelia/logging@0.7.0",
       "aurelia-metadata": "github:aurelia/metadata@0.8.0",
+      "core-js": "npm:core-js@0.9.18"
+    },
+    "github:aurelia/dependency-injection@0.11.2": {
+      "aurelia-logging": "github:aurelia/logging@0.8.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.9.0",
+      "aurelia-pal": "github:aurelia/pal@0.2.0",
       "core-js": "npm:core-js@0.9.18"
     },
     "github:aurelia/event-aggregator@0.8.0": {
@@ -101,6 +114,10 @@ System.config({
       "aurelia-loader": "github:aurelia/loader@0.9.0",
       "aurelia-metadata": "github:aurelia/metadata@0.8.0"
     },
+    "github:aurelia/loader@0.10.0": {
+      "aurelia-metadata": "github:aurelia/metadata@0.9.0",
+      "aurelia-path": "github:aurelia/path@0.10.0"
+    },
     "github:aurelia/loader@0.9.0": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.3.0",
       "aurelia-metadata": "github:aurelia/metadata@0.8.0",
@@ -111,6 +128,10 @@ System.config({
       "aurelia-logging": "github:aurelia/logging@0.7.0"
     },
     "github:aurelia/metadata@0.8.0": {
+      "core-js": "npm:core-js@0.9.18"
+    },
+    "github:aurelia/metadata@0.9.0": {
+      "aurelia-pal": "github:aurelia/pal@0.2.0",
       "core-js": "npm:core-js@0.9.18"
     },
     "github:aurelia/route-recognizer@0.7.0": {
@@ -125,6 +146,9 @@ System.config({
       "aurelia-path": "github:aurelia/path@0.9.0",
       "aurelia-route-recognizer": "github:aurelia/route-recognizer@0.7.0",
       "core-js": "npm:core-js@0.9.18"
+    },
+    "github:aurelia/task-queue@0.8.0": {
+      "aurelia-pal": "github:aurelia/pal@0.2.0"
     },
     "github:aurelia/templating-binding@0.15.0": {
       "aurelia-binding": "github:aurelia/binding@0.9.1",
@@ -158,6 +182,24 @@ System.config({
       "aurelia-path": "github:aurelia/path@0.9.0",
       "aurelia-task-queue": "github:aurelia/task-queue@0.7.0",
       "core-js": "npm:core-js@0.9.18"
+    },
+    "github:aurelia/templating@0.16.0": {
+      "aurelia-binding": "github:aurelia/binding@0.10.2",
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.11.2",
+      "aurelia-loader": "github:aurelia/loader@0.10.0",
+      "aurelia-logging": "github:aurelia/logging@0.8.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.9.0",
+      "aurelia-pal": "github:aurelia/pal@0.2.0",
+      "aurelia-path": "github:aurelia/path@0.10.0",
+      "aurelia-task-queue": "github:aurelia/task-queue@0.8.0",
+      "core-js": "npm:core-js@0.9.18"
+    },
+    "github:aurelia/validation@0.4.0": {
+      "aurelia-binding": "github:aurelia/binding@0.10.2",
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.11.2",
+      "aurelia-logging": "github:aurelia/logging@0.8.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.9.0",
+      "aurelia-templating": "github:aurelia/templating@0.16.0"
     },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -233,7 +275,7 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-runtime@5.8.20": {
+    "npm:babel-runtime@5.8.29": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:browserify-aes@1.0.5": {
@@ -300,6 +342,12 @@ System.config({
     },
     "npm:core-js@0.9.18": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:core-js@1.2.3": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ export function configure(aurelia) {
     .standardConfiguration()
     .developmentLogging();
   //Uncomment the line below to enable animation.
-  //aurelia.use.plugin('aurelia-animator-css');
+  aurelia.use.plugin('aurelia-animator-css');
+  aurelia.use.plugin('aurelia-validation');
 
   //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   //aurelia.use.plugin('aurelia-html-import-template-loader')
